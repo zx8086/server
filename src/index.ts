72b2d616c6d07056b70ea518ebb21c3d36986509
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2023-11-08 21:50:15 +0100

    Wierd commit

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,59 @@
 import { myApm} from "./initapm";
 
 import { Elysia } from "elysia";
+import { apollo, gql } from '@elysiajs/apollo'
 
 import { setCommonHeaders } from "./lib/utils";
 import { controllers } from "./lib/controllers";
 
 const app = new Elysia()
-  .use(controllers)
-  .onRequest(({ request }) => {
-    const url = new URL(request.url)
-    // console.log(`📩 <-- ${request.method} ${url.pathname}`)
-    // myApm.setTransactionName(`${request.method} ${url.pathname}`);
-    myApm.startTransaction();
-    // myApm.startTransaction(`${request.method} ${url.pathname}`, 'request');
-    // myApm.setTransactionName(`${request.method} ${url.pathname}`);
-    // apm.logger.info(`APM 📩 <-- ${request.method} ${url.pathname}`);
-  })
-  // .onResponse(()=> {
-  //     // const url = new URL(ctx.request.url);  // <-- Define the `url` variable within this function
-  //     // console.log(`${typeof ctx.set.status === 'number' ? emojiStatus.get(ctx.set.status) ?? "" : ""} --> ${ctx.request.method} ${ctx.request.path} ${ctx.set.status}`)
-  //   })
-  .onAfterHandle(() => {
-    myApm.endTransaction();
-    // myApm.flush();
-  })
-  // .use(setCommonHeaders)
-  .listen(3000);
+  .use(
+    apollo({
+        typeDefs: gql`
+            type Book {
+                title: String
+                author: String
+            }
+
+            type Query {
+                books: [Book]
+            }
+        `,
+        resolvers: {
+            Query: {
+                books: () => {
+                    return [
+                        {
+                            title: 'Elysia',
+                            author: 'saltyAom'
+                        }
+                    ]
+                }
+            }
+        }
+    })
+  )
+  .listen(8080);
+  // .use(controllers)
+  // .onRequest(({ request }) => {
+  //   const url = new URL(request.url)
+  //   // console.log(`📩 <-- ${request.method} ${url.pathname}`)
+  //   // myApm.setTransactionName(`${request.method} ${url.pathname}`);
+  //   myApm.startTransaction();
+  //   // myApm.startTransaction(`${request.method} ${url.pathname}`, 'request');
+  //   // myApm.setTransactionName(`${request.method} ${url.pathname}`);
+  //   // apm.logger.info(`APM 📩 <-- ${request.method} ${url.pathname}`);
+  // })
+  // // .onResponse(()=> {
+  // //     // const url = new URL(ctx.request.url);  // <-- Define the `url` variable within this function
+  // //     // console.log(`${typeof ctx.set.status === 'number' ? emojiStatus.get(ctx.set.status) ?? "" : ""} --> ${ctx.request.method} ${ctx.request.path} ${ctx.set.status}`)
+  // //   })
+  // .onAfterHandle(() => {
+  //   myApm.endTransaction();
+  //   // myApm.flush();
+  // })
+  // // .use(setCommonHeaders)
+  // .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
